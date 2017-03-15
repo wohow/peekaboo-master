96208ac22679e966cd9812867475f6f4424acbe4
wohow <121535512@qq.com>
2017-03-15 09:49:29 +0800

    1

--- peekaboo-client/assets/script/custom-component/Atlas.js
+++ peekaboo-client/assets/script/custom-component/Atlas.js
@@ -1,4 +1,5 @@
 
+// 这个只对tilemap有效
 cc.macro.FIX_ARTIFACTS_BY_STRECHING_TEXEL = 1;
 
 cc.Class({
@@ -9,6 +10,7 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        
+    	// 关闭抗锯齿 设置一次就可以了
+        cc.view.enableAntiAlias(false);
     },
 });
